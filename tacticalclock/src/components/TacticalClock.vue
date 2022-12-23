fbf4c44009177e13b358ba60b06c32485eac7e4e
Meier, Ulrich (LDBV) <ulrich.meier@ldbv.bayern.de>
2022-12-23 18:38:11 +0100

    tClock: all values

--- tacticalclock/src/components/TacticalClock.vue
+++ tacticalclock/src/components/TacticalClock.vue
@@ -1,10 +1,6 @@
 <template>
 <div class="container">
-  <div>{{hours}}</div>
-  <div>:</div>
-  <div>{{minutes}}</div>
-  <div>:</div>
-  <div>{{seconds}}</div>
+  <div>{{days}}{{hours}}{{minutes}}{{months}}{{years}}</div>
 </div>
 </template>
 
@@ -13,22 +9,30 @@ export default {
   name: "TacticalClock",
   data () {
     return {
+      days: 0,
       hours: 0,
       minutes: 0,
-      seconds: 0,
+      months: "",
+      years: ""
     }
   },
   methods: {
     setTime() {
       setInterval(() => {
         const date = new Date()
+        this.days = date.getDate()
         this.hours = date.getHours()
         this.minutes = this.checkSingleDigit(date.getMinutes())
-        this.seconds = this.checkSingleDigit(date.getSeconds())
+        this.months = this.getMonthText(date.getMonth())
+        this.years = date.getFullYear().toString().slice(-2)
       }, 1000)
     },
     checkSingleDigit (digit) {
       return ('0' + digit).slice(-2)
+    },
+    getMonthText (digit) {
+      const month = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+      return month[digit]
     }
   },
   mounted() {

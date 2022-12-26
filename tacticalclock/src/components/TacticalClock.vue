df70bc6fe19b7f454c4c6577033701258b72e516
Uli Meier <uli@meier-tkn.de>
2022-12-26 01:50:27 +0100

    Sekunden und Zeitzone

--- tacticalclock/src/components/TacticalClock.vue
+++ tacticalclock/src/components/TacticalClock.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
     <div class="tclock">
-      <div>{{ days }}<span class="time">{{ hours }}{{ minutes }}</span>{{ months }}{{ years }}</div>
+      <div v-if="years">{{ days }}<span class="time">{{ hours }}{{ minutes }}</span><span class="seconds">{{ seconds }}</span><span class="tz">{{ timezone }}</span>{{ months }}{{ years }}</div>
+      <div v-else>Initializing</div>
     </div>
   </div>
 </template>
@@ -14,8 +15,10 @@ export default {
       days: 0,
       hours: 0,
       minutes: 0,
+      seconds: 0,
       months: "",
-      years: ""
+      years: "",
+      timezone: ""
     }
   },
   methods: {
@@ -25,20 +28,30 @@ export default {
         this.days = this.checkSingleDigit(date.getDate())
         this.hours = this.checkSingleDigit(date.getHours())
         this.minutes = this.checkSingleDigit(date.getMinutes())
+        this.seconds = this.checkSingleDigit(date.getSeconds())
         this.months = this.getMonthText(date.getMonth())
         this.years = date.getFullYear().toString().slice(-2)
+        this.timezone = this.getTimeZone(date.getTimezoneOffset())
       }, 1000)
     },
     checkSingleDigit(digit) {
       return ('0' + digit).slice(-2)
     },
     getMonthText(digit) {
-      const month = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
-      return month[digit]
-    }
+      return this.month[digit]
+    },
+    getTimeZone(number) {
+      let index = 12 + (number / 60 * -1)
+      return this.zones[index]
+    },
   },
+
   mounted() {
     this.setTime()
+  },
+  created() {
+    this.month = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+    this.zones = ["Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M"]
   }
 }
 </script>
@@ -55,5 +68,12 @@ export default {
 }
 .time {
   font-size: larger;
+}
+.seconds {
+  font-size: smaller;
+  color: gray;
+}
+.tz {
+  color: gray;
 }
 </style>
